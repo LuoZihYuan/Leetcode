0fe003c332a3339b3eec723975e5d3e960e09088
LuoZihYuan <zihyuan.luo@gmail.com>
2024-09-15 11:07:25 -0700

    :sparkles: feat: 0027 Solved

---
+++ 01 Array/01-02_L0027.cpp
@@ -0,0 +1,66 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+/* Your implementation of `Solution` goes here. */
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int j = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] != val) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+};
+/* Uncomment the following code snippet on Leetcode for performance optimization. */
+// int init = [] {
+//     ios_base::sync_with_stdio(0);
+//     cin.tie(0);
+//     ofstream out("user.out");
+//     cout.rdbuf(out.rdbuf());
+// 
+//     for (string s; getline(cin, s); cout << '\n') {
+//         string t;
+//         getline(cin, t);
+//         int tar = stoi(t);
+// 
+//         for (int i = 0, _i = 1, _n = s.length(); _i < _n; ++i, ++_i) {
+//             bool _neg = 0;
+//             if (s[_i] == '-')
+//                 ++_i, _neg = 1;
+//             int v = s[_i++] & 15;
+//             while ((s[_i] & 15) < 10)
+//                 v = v * 10 + (s[_i++] & 15);
+//             if (_neg)
+//                 v = -v;
+//             if (v == tar) {
+//                 cout << i;
+//                 goto next;
+//             }
+//             if (v > tar)
+//                 break;
+//         }
+//         cout << -1;
+//     next:;
+//     }
+//     exit(0);
+//     return 0;
+// }();
+
+int main() {
+
+    int ret;
+
+    vector<int> case_1 = {3,2,2,3};
+    ret = Solution().removeElement(case_1, 3);
+    cout << ret << endl;
+    vector<int> case_2 = {0,1,2,2,3,0,4,2};
+    ret = Solution().removeElement(case_2, 2);
+    cout << ret << endl;
+    
+    return 0;
+}
