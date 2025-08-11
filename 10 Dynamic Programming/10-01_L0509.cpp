6084898bd932fd7e5107dd6ce1d013cca6cf27d4
LuoZihYuan <zihyuan.luo@gmail.com>
2025-08-11 16:11:51 +0800

    :sparkles: feat: 0509 Solved

---
+++ 10 Dynamic Programming/10-01_L0509.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int fib(int n) {
+        if (n < 2) return n;
+        int a = 0, b = 1;
+        for (int i = 1; i < n; i++){
+          int sum = a + b;
+          a = b;
+          b = sum;
+        }
+        return b;
+    }
+};
+
+int main() {
+
+  return 0;
+}
