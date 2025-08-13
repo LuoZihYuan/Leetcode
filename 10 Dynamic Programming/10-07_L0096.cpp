3cf284ae3f776b9775200c3ae9b927e260abc27a
LuoZihYuan <zihyuan.luo@gmail.com>
2025-08-13 21:24:36 +0800

    :sparkles: feat: 0096 Solved

---
+++ 10 Dynamic Programming/10-07_L0096.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> dp(n+1, 0);
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++) {
+          for(int j = 0; j < i; j++) {
+            dp[i] += dp[j] * dp[i-j-1];
+          }
+        }
+        return dp[n];
+    }
+};
+
+int main() {
+  cout << Solution().numTrees(3) << endl;
+  cout << Solution().numTrees(1) << endl;
+  return 0;
+}
